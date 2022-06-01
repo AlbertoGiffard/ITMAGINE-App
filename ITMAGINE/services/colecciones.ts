0e1ca6dfde7106540a716057a1d21a0907bf99c1
Leandro Varela Vargas <lvarela@certant.com>
2022-06-01 02:37:07 -0300

    Creadas constantes con nombres de colecciones en firestore

---
+++ ITMAGINE/services/colecciones.ts
@@ -0,0 +1,5 @@
+export const COLECCION_DUENIO = "Duenio";
+export const COLECCION_EMPLEADOS = "Empleados";
+export const COLECCION_MESAS = "Mesas";
+export const COLECCION_PRODUCTOS = "Productos";
+export const COLECCION_CLIENTES = "Clientes";
