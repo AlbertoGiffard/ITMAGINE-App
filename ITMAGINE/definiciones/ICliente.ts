d04f15d729896a61373290792d003fc55b56faa0
Leandro Varela Vargas <lvarela@certant.com>
2022-06-01 13:56:00 -0300

    Documentado de tipos y agregados atributos a ICliente

--- ITMAGINE/definiciones/ICliente.ts
+++ ITMAGINE/definiciones/ICliente.ts
@@ -1,9 +1,30 @@
-declare type EstadoCliente = ""
+import { IEmpleado } from "./IEmpleado";
+
+/**
+ * Estados que puede tomar la administración de la cuenta.
+ *  en proceso: Cuenta recién creada y esperando aprobación.
+ *  aceptada: **
+ *  rechazada: **
+ */
+declare type EstadoCuentaCliente = "en proceso" | "aceptada" | "rechazada" ;
+
+/**
+ * Estados que puede tomar un cliente respecto a su presencia en el restaurante:
+ *  sentado: En Mesa esperando a ser atendido.
+ *  ordenando: En Mesa ya atendido.
+ *  esperando: Esperando Orden.
+ *  comiendo: **
+ *  encuestado: Habiendo pagado y respondiendo encuesta. Si desea no responderla pasa a ausente.
+ *  ausente: No se encuentra en el establecimiento.
+ */
+declare type EstadoAtencionCliente = "sentado" | "ordenando" | "esperando" | "comiendo" | "encuestado" | "ausente";
 
 export declare interface ICliente {
     nombre : string,
     apellido : string,
     DNI : string,
     fotoURL : string,
-    estado : EstadoCliente
+    estadoCuenta : EstadoCuentaCliente,
+    estadoAtencion: EstadoAtencionCliente,
+    atendidoPor? : IEmpleado
 }
