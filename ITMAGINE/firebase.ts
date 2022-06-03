7581d81365b91759c0ed49e79d3798124caab3f2
Agustin Clas <agustinclasst@gmail.com>
2022-06-03 20:53:41 -0300

    subo login

---
+++ ITMAGINE/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB89Y_kGMNDRBf-c8K57KBOlqYk6khl8Uw",
+  authDomain: "michat-f7b9c.firebaseapp.com",
+  projectId: "michat-f7b9c",
+  storageBucket: "michat-f7b9c.appspot.com",
+  messagingSenderId: "1008156798984",
+  appId: "1:1008156798984:web:6686b30e3124053df2e1bd"
+};
+
+// Initialize Firebase
+let app;
+/* if(firebase.getApps().lenth === 0){
+    app = initializeApp(firebaseConfig);
+}
+else{ 
+    app = firebase.getAuth();
+}*/
+
+app = firebase.initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export const database = getFirestore();
+
+export {auth} ; 
