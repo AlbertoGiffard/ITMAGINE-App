ba026b0dc06c447c3241a71f927d28efcec96786
Leandro Varela Vargas <lvarela@certant.com>
2022-06-07 10:15:05 -0300

    Agregado Context a Aplicación

--- ITMAGINE/App.tsx
+++ ITMAGINE/App.tsx
@@ -1,9 +1,14 @@
 import { LogBox } from 'react-native';
 import AppNavegacion from './componentes/navegacion/AppNavegacion';
+import { AppContext } from './context/AppContext';
 
 LogBox.ignoreLogs(["AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage", "Non-serializable values were found in the navigation state."]);
 
 export default function App() {
   //siempre debe renderizar hacia esta ruta 
-  return <AppNavegacion/>
+  return (
+  <AppContext.Provider value={null}>
+    <AppNavegacion/>
+  </AppContext.Provider>
+  );
 }
