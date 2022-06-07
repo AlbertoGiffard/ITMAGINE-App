8be92f31f0ca90012110b1175509b16b56d3adee
Leandro Varela Vargas <lvarela@certant.com>
2022-06-07 11:07:18 -0300

    Creadas constantes para colores comunes en toda la app

---
+++ ITMAGINE/estilos/estilo.ts
@@ -0,0 +1,4 @@
+export const PRIMARY_COLOR = "#fd99ef"; // Rosadito
+export const SECONDARY_COLOR = "#3dd7fb"; // Azulito
+export const TERCIARY_COLOR = "#ffe045"; // Amarillo
+export const BG_COLOR = "#040104"; // Negro de Fondo
