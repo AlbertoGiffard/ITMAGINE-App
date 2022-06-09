597172d4ec6f048845dd01a6d0bca0cd3d00cccf
Leandro Varela Vargas <lvarela@certant.com>
2022-06-09 15:49:24 -0300

    Creado archivo globalStyles que contiene constantes de parámetros de estilizado que se utilizan comúnmente

---
+++ ITMAGINE/estilos/globalStyle.ts
@@ -0,0 +1,9 @@
+import { Dimensions } from "react-native";
+
+export const PRIMARY_COLOR = "#fd99ef"; // Rosadito
+export const SECONDARY_COLOR = "#3dd7fb"; // Azulito
+export const TERCIARY_COLOR = "#ffe045"; // Amarillo
+export const BG_COLOR = "#040104"; // Negro de Fondo
+
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
